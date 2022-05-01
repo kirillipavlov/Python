7624a2ccb37cb8b5cca8b2f6896bff0ebbb48b9c
kirillipavlov <kirill.i.pavlov@gmail.com>
2022-05-01 05:22:05 +0300

    commit

--- Обучение/времена года.py
+++ Обучение/времена года.py
@@ -2,11 +2,11 @@ print("Введите номер месяца от 1 до 12")
 number = int(input())
 if number >= 3 and number <= 5:
     print("Весна")
-elif number >=6 and number <=8:
+elif number >= 6 and number <= 8:
     print("Лето")
-elif number >=9 and number <=11:
+elif number >= 9 and number <= 11:
     print("Осень")
-elif number >=1 and number <=2:
+elif number >= 1 and number <= 2:
     print("Зима")
 elif number == 12:
     print("Зима")
